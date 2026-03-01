e64cfcdfc8ac9b3866d91d8750214c067450fcf3
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2026-03-01 20:00:30 +0530

    feat: scaffolding for admin panel

--- backend/routes/messages/views.py
+++ backend/routes/messages/views.py
@@ -16,7 +16,8 @@ from datetime import datetime, timezone
 
 router = APIRouter()
 
-#TODO: move this to redis
+
+# TODO: move this to redis
 class MessagesConnectionManager:
     active_connections = dict[UUID, list[WebSocket]]
 
